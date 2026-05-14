45ddd10aa9c5e05dc81d0d6c5c9e9b3f6afbcb3e
HadiHeikal <hadiiheikal@gmail.com>
2026-05-14 14:55:43 +0300

    Fix HomePage constructor to be a constant constructor

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../task/add_task_view.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
